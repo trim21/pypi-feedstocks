2c9a9f41867243b47aa578c4797ddfc01c8d7aaa
Trim21 <trim21.me@gmail.com>
2025-04-10 07:39:11 +0800

    typo

--- ci/upload.bash
+++ ci/upload.bash
@@ -3,5 +3,5 @@ ls dist -ahl
 if [ $? -eq 0 ]; then
   rattler-build upload prefix -c pypi-mirrors dist/*.conda
 else
-  echo "on packages, skip uploading"
+  echo "no new packages, skip uploading"
 fi
