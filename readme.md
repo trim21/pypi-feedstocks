96dd0c3cd186065c8f53b8cd382df66abd013ef2
Trim21 <trim21.me@gmail.com>
2025-04-10 07:44:42 +0800

    add readme

--- readme.md
+++ readme.md
@@ -3,3 +3,12 @@
 release some pypi packages to conda ecosystem
 
 <https://prefix.dev/channels/pypi-mirrors>
+
+## packages
+
+|       pypi       | version  |
+| :--------------: | :------: |
+|    durationpy    |   0.9    |
+| qbittorrent-api  | 2025.2.0 |
+|      sslog       | 0.0.0a52 |
+| transmission-rpc |  7.0.11  |
--- render.py
+++ render.py
@@ -10,9 +10,29 @@ packages = project_root.joinpath("packages")
 outputs = []
 
 for pkg in packages.iterdir():
+    if not pkg.name.endswith(".yaml"):
+        continue
     outputs.append(yaml.safe_load(pkg.read_bytes()))
 
 
 outputs.sort(key=lambda x: x["context"]["name"])
 
 project_root.joinpath("recipe.yaml").write_text(yaml.dump({"outputs": outputs}))
+
+
+readme = project_root.joinpath("readme.in").read_text("utf-8").strip()
+
+
+lines = [
+    "",
+    "| pypi | version |",
+    "| :--: | :-----: |",
+]
+
+for pkg in outputs:
+    lines.append("| {name} | {version} |".format_map(pkg["context"]))
+
+
+project_root.joinpath("readme.md").write_text(
+    readme + "\n".join(lines), encoding="utf-8"
+)
