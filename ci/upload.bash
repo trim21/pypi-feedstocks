ad1f458e8f5db111b443882521a5f1fb8230619e
Trim21 <trim21.me@gmail.com>
2025-04-27 03:44:19 +0800

    upload to anaconda

--- ci/upload.bash
+++ ci/upload.bash
@@ -2,6 +2,7 @@ ls dist -ahl
 
 if [ $? -eq 0 ]; then
   rattler-build upload prefix -c pypi-mirrors dist/*.conda
+  rattler-build upload anaconda -o pypi-mirrors dist/*.conda
 else
   echo "no new packages, skip uploading"
 fi
